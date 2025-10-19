a5f0b8daac89328488af1307ae22a8ca04a076d8
Mario <marioc720@gmail.com>
2025-10-19 01:22:42 -0300

    estiliza pageMovies como pageSeries

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,26 +1,41 @@
 import React from 'react'
 import useEntries from "../hooks/useEntries"
+import MoreInfoCard from '../components/MoreInfoCard'
 
 export default function Movies() {
+  const { data, loading, error } = useEntries("series")
+  const [selectedItem, setSelectedItem] = useState(null)
 
-  const { data, loading, error } = useEntries("movie")
-    
-    if (loading) {return
+  const handleOpenPopup = (item) => {
+    setSelectedItem(item);
+  };
+  const handleClosePopup = () => {
+    setSelectedItem(null);
+  };
+
+
+
+
+  if (loading) {
+    return
     <>
       <p className='bg-[#414141] text-sm text-white py-2 sm:pl-10 pl-2 md:pl-20 sm:text-lg font-medium '>
         Popular Movies
       </p>
       <p className="text-center text-gray-600 pl-4 sm:pl-10 md:pl-20">Loading...</p>;
-    </>}
-  
-    if (error){return
+    </>
+  }
+
+  if (error) {
+    return
     <>
       <p className='bg-[#414141] text-sm text-white py-2 sm:pl-10 pl-2 md:pl-20 sm:text-lg font-medium '>
         Popular Movies
       </p>
       <p className="text-center text-red-500 p-8">Oops, something went wrong...</p>;
-    </>}
-  
+    </>
+  }
+
 
 
   return (
@@ -28,15 +43,18 @@ export default function Movies() {
       <p className='bg-[#414141] text-sm text-white py-2 sm:pl-10 pl-2 md:pl-20 sm:text-lg font-medium '>
         Popular Movies
       </p>
-      <div className="grid grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-2 px-4 sm:px-10 md:px-20 py-4">
+      <div className="relative w-full bg-gray-50 grid grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-2 px-4 sm:px-10 md:px-20 py-4">
         {data.map((item) => (
           <div key={item.title} className='flex flex-col justify-start gap-2 pb-4'>
-          <div className="bg-gray-800 rounded overflow-hidden shadow-lg w-30 h-46">
-            <img src={item.images["Poster Art"].url} alt={item.title} className="w-full object-cover" />
-          </div>
-          <h3 className="font-semibold text-lg">Program {item.title}</h3>
+            <div onClick={() => handleOpenPopup(item)} className="bg-gray-100 hover:opacity-60 hover:border-2 hover:border-white rounded overflow-hidden shadow-lg w-30 h-46 cursor-pointer">
+              <img src={item.images["Poster Art"].url} alt={item.title} className="w-full object-cover" />
+            </div>
+            <h3 className="font-semibold text-xs text-start w-30 text-gray-500" onClick={() => handleOpenPopup(item)}>Program
+              <button className='pl-2 cursor-pointer hover:font-bold text-gray-500 active:text-gray-900'>{item.title}</button>
+            </h3>
           </div>
         ))}
+        <MoreInfoCard />
       </div>
     </>
   )
